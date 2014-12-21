e49bf0428932ee9d1d0e01ab6e2f4a2781d5c97d
Keksike <cihan.bebek@hotmail.com>
2014-12-21 04:14:34 +0200

    commenting etc.

--- js/getQuotes.js
+++ js/getQuotes.js
@@ -6,15 +6,19 @@ var _ = require('lodash');
 var $ = require('jquery');
 
 
-//used for storing id's
+// tells us which id we last checked
 var quoteIdCounter = 0;
 
-/*amount = amount of fetched quotes and created quoteDivs*/
+/* amount = amount of fetched quotes and created quoteDivs */
 function getQuotes(amount){
     for (var i = 0; i < amount; i++) {
-        setTimeout(function(){
+        setTimeout(function(){ // setTimeout to fix spamming down, I guess. Just worried about lag in node/db
+            //gets a single quote with given id (autoincremented) from db
             $.get(("/quotes/" + quoteIdCounter), function(data) {
-                if(data != null){
+                if(data != null){ //if we found a quote with id
+
+                    // gotta do something about this mess of a code. 
+                    // should probably move it to another function too.
                     var lineString = $('<div class="line"></div>');
                     var quoteString = $('<div class="quoteDiv">' 
                                             + '<p class="time">' + data.time + '</p>' + '<br>'
@@ -26,37 +30,28 @@ function getQuotes(amount){
                     $("#quotes").append(quoteString);
                 }
             }, "json" );
+            // increments no matter if we found quote or not
             quoteIdCounter++;
         }, 500);
     };
 };
 
-function checkScrollBar() {
-    var hContent = $("body").height(); // get the height of your content
-    var hWindow = $(window).height();  // get the height of the visitor's browser window
 
-    // if the height of your content is bigger than the height of the 
-    // browser window, we have a scroll bar
-    if(hContent>hWindow) { 
-        return true;    
-    }
-
-    return false;
-}
-
-/*when page loads, make sure to load some quotes*/
+/* when page loads, make sure to load some quotes
+   I'm fairly sure that theres a better/faster way of doing this, but this seems safe and it works so meh.*/
 $(function(){
     var hContent = $("body").height();
     var hWindow = $(window).height();
 
+    // loads single quotes until we have a scroll bar
     while(hContent<=hWindow){
         _.throttle(getQuotes(1), 1000);
     }
 });
 
-/*gets quotes when scrolling page down*/
+/* gets quotes when scrolling page down */
 window.onscroll = function() {
-    //when scrolling page checks if you've hit the bottom
+    // when scrolling page checks if you've hit the bottom
     if (((window.innerHeight + window.scrollY) >= document.body.offsetHeight)) {
         _.throttle(getQuotes(5), 1000);
     }
